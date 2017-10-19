833e4a2acab43f5743cfc856e419a50af301fa35
Steven Orola <stevenorola@gmail.com>
2017-10-19 12:00:32 -0700

    finished job 9

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -23,7 +23,7 @@ class Application extends CI_Controller
 
 		//  Set basic view parameters
 		$this->data = array ();
-		$this->data['pagetitle'] = 'Comp4711 Lab05 Group 6';
+		$this->data['pagetitle'] = 'Comp4711 Lab06 Group 6';
 		$this->data['ci_version'] = (ENVIRONMENT === 'development') ? 'CodeIgniter Version <strong>'.CI_VERSION.'</strong>' : '';
 	}
 
