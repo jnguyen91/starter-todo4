ecc4d243b68b0c7f1a070267288332a0e1678791
Jeffrey Nguyen <jeffnguyen91@hotmail.com>
2017-10-22 12:34:13 -0700

    template secondary cleanuo

--- application/views/template_secondary.php
+++ application/views/template_secondary.php
@@ -20,11 +20,11 @@
 				<h3>Tasks by Category</h3>
 				<div class = "row">
 					<!--<div class = "col-lg-6 col-md-12">-->
-                                        <div class ="span6"
+                                        <div class ="span6">
 					{leftside}
 				</div>
 					<!--<div class = "col-lg-6 col-md-12">-->
-                                        <div class ="span6"
+                                        <div class ="span6">
 					{rightside}
 				</div>
             </div>
